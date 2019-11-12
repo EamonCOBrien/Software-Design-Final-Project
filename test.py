1b8e5a4358ae3f404decb2df5a40ed3ea17a8446
Eamon O'Brien <eamon@piratecat.com>
2019-11-11 22:01:21 -0500

    Change port and host

--- test.py
+++ test.py
@@ -45,4 +45,6 @@ if __name__ == '__main__':
     model = Model()
     view = View(model)
     controller = Controller(model)
-    app.run()
+    HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
+    PORT = int(os.environ.get('PORT', 5000))
+    app.run(host=HOST, port=PORT)
